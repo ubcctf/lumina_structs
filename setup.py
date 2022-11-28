b6d36c9a7b1e93b9b2128ca074cca1ad1b98a19e
despawningbone <despawningbone@gmail.com>
2022-11-27 21:19:39 -0800

    Setup module correctly for loading

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup(name='lumina_structs',
       version='0.1',
       description='Protocol definitions for Lumina',
       author='Synacktiv',
-      maintainer='Maple Bacon')
+      maintainer='Maple Bacon',
+      packages=['lumina_structs'],
+      package_dir={
+        'lumina_structs': '.',
+      },
+      include_package_data=True,
+      exclude_package_data={
+        '': ['setup.py']
+      })
