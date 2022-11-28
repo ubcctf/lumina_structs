9a42d9007140ac2f4acfa3536ed668d44165a5c3
despawningbone <despawningbone@gmail.com>
2022-11-27 20:54:22 -0800

    Make module installable

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='lumina_structs',
+      version='0.1',
+      description='Protocol definitions for Lumina',
+      author='Synacktiv',
+      maintainer='Maple Bacon')
